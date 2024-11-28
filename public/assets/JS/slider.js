4d3c7231077b92fa4f377900146d9e2f45422aef
MiguelCSerpa <miguel.serpa@sptech.school>
2024-11-27 22:16:10 -0300

    Comentando

--- public/assets/JS/slider.js
+++ public/assets/JS/slider.js
@@ -1,11 +1,11 @@
 var sliderDiv = document.querySelector(".slider-container");
 var seletores = document.querySelectorAll(".seletor");
-var elements = document.querySelectorAll(".slide-element");
-var slideWidth = sliderDiv.scrollWidth / elements.length;
+var elementos = document.querySelectorAll(".slide-element");
+var slideWidth = sliderDiv.scrollWidth / elementos.length; // calcula a largura de cada slider para que ele possa deslizar
 
-for (let i = 0; i < seletores.length; i++) {
+for (let i = 0; i < seletores.length; i++) { // percorre todos os elementos dos seletores
     seletores[i].addEventListener("click", (event) => {
-        event.preventDefault();
+        // event.preventDefault();
 
         sliderDiv.scrollLeft = i * slideWidth;
     });
